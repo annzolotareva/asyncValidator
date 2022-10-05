8862f96c6d26e401360e4ac47afd67cd107ea0b5
Золотарева Анна Александровна <aazolotareva@phoenixit.ru>
2022-10-05 16:31:57 +0300

    Изменения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'asyncValidator';
 
-  ngOninit() {
+  ngOnInit() {
     async function f() { 
     let sum = [1, 2, 3, 4, 5, 6].reduce(function(sum, elem) {
       return sum + elem;
     }, 0);
-    let promise = new Promise((resolve, reject) => {
+    let promise = await new Promise((resolve, reject) => {
       setTimeout(() => resolve(sum), 3000)
     });
     let result = await promise; // будет ждать, пока промис не выполнится (*)
