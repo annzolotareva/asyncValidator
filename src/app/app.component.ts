838c2d0509c0ec54523353f40ec85c74a2703d01
Золотарева Анна Александровна <aazolotareva@phoenixit.ru>
2022-10-05 11:01:37 +0300

    Новые изменения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asyncValidator';
+
+  ngOninit() {
+    async function f() { 
+    let sum = [1, 2, 3, 4, 5, 6].reduce(function(sum, elem) {
+      return sum + elem;
+    }, 0);
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(sum), 3000)
+    });
+    let result = await promise; // будет ждать, пока промис не выполнится (*)
+	  console.log(result); // "готово!"
+  }
+  
+  f();
+  }
+
 }
